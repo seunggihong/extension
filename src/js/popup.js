aaa7c9a2bf442552ade40b7c63bc1d1562e9710b
onggi <hsk1533@naver.com>
2022-05-10 20:44:22 +0900

    env

--- src/js/popup.js
+++ src/js/popup.js
@@ -2,6 +2,8 @@ const count = document.getElementById("count");
 const btn = document.getElementById("btn");
 const reset = document.getElementById("reset");
 
+const githubApi = "https://api.github.com/";
+
 btn.onclick = () => {
   const current = parseInt(count.innerText, 10);
   count.innerText = current + 1;
